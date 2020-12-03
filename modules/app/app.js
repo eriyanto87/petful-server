9ecfecc5a9762750037e16301450bef2f2939e45
Evisari Riyanto <evisari111012@gmail.com>
2020-12-03 14:59:41 -0800

    added app.get path

--- modules/app/app.js
+++ modules/app/app.js
@@ -8,4 +8,8 @@ app.use(cors());
 app.use("/", require("../people/people.router"));
 app.use("/", require("../pets/pets.router"));
 
+app.get("/", (req, res) => {
+  res.send("Hello from Petful!");
+});
+
 module.exports = app;
